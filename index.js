392e7b1eb9ea148814a4d813a69dce58687aaa11
Anu Jose <anujose1989@gmail.com>
2017-10-04 22:21:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,10 @@ function logWhisper(string){
 logWhisper("HELLO");
 
 function sayHiToGrandma(string){
-    if (string.toLowerCase() == true){
+    if (string.toLowerCase() === true){
       return "I can't hear you!";
     }
-    else if (string.toUpperCase() == true){
+    else if (string.toUpperCase() === true){
       return "YES INDEED!"
     }
     else if (string === "I love you, Grandma."){
